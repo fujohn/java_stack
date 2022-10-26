bab0405719292d1351946083e8382a8357890295
John Fu <fu.johnyh@gmail.com>
2022-10-25 18:09:57 -0700

    cafe java assignment

--- fundamentals/greetings/Test.java
+++ fundamentals/greetings/Test.java
@@ -2,5 +2,59 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int ourInt; // we can declare a variable without setting its value
+        ourInt = 400; // we can assign a value to the variable later in our code
+        double pi = 3.14159265; // we can also declare and assign on the same line
+        boolean bool = true;
+        char singleCharacter = 'A';
+        
+        String multipleCharacters = "ABC";
+
+        // conditionals
+        boolean isRaining = true;
+        boolean isCold = true;
+            
+        if(isRaining) {
+            System.out.println("Bring an umbrella.");
+        }
+        else if(isCold) {
+            System.out.println("Wear a coat.");
+        }
+        else {
+            System.out.println("Have fun!");
+        }
+
+        // switch
+        int day = 2;
+        switch(day){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("There are only seven days in a week...");
+        }
+
+        
+        // Ternary (logical ? if_true : if_false)
+        System.out.println(isRaining ? "Bring an umbrealla" : "Have fun!");
+
     }
 }
