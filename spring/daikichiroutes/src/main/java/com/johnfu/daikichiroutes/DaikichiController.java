594dc897102ee43cd1f26b2b4d47b842b2dddf34
John Fu <fu.johnyh@gmail.com>
2022-11-11 17:04:33 -0800

    Daikichi part 2

--- spring/daikichiroutes/src/main/java/com/johnfu/daikichiroutes/DaikichiController.java
+++ spring/daikichiroutes/src/main/java/com/johnfu/daikichiroutes/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.johnfu.daikichiroutes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +21,18 @@ public class DaikichiController {
 	public String tomorrow() {
 		return "Tomorrow, an opportunitiy will arise, so be sure to be open to new ideas!";
 	}
+	
+	@RequestMapping("/travel/{location}")
+	public String location(@PathVariable("location") String location){
+    	return "Congratulations! You will soon travel to " + location + "!";
+    }
+	
+	@RequestMapping("/lotto/{number}")
+	public String location(@PathVariable("number") int number){
+    	if (number % 2 == 0) {
+    		return "You will take a grand journey in the near future, but be weary of tempting offers.";
+    	} else {
+    		return "You have enjoyed the fruits of your labor but now is a great time with family and friends.";
+    	}
+    }
 }
